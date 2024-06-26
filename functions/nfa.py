f84f17912f76ad95e090b07bc6ee993d9aa404c9
Leonardo Morabito <leonardomorabito02@gmail.com>
2024-06-26 16:28:55 -0300

    cambio e por ε comento nfa.py

--- functions/nfa.py
+++ functions/nfa.py
@@ -1,16 +1,9 @@
 from graphviz import Digraph
-from pprint import pprint
 from functions.nodes import Or
-from functions.tokens import TokenType
 
-def WriteToFile(filename: str, content: str):
-    with open(filename, 'w') as _file:
-        _file.write(content)
-
-    return f'Archivo "{filename}" creado'
 class NFA:
     def __init__(self, tree, symbols, regex):
-        # Propiedades de un autómata finito
+        # Propiedades del autómata finito no determinista (NFA)
         self.accepting_states = []
         self.symbols = symbols
         self.trans_func = None
@@ -21,33 +14,34 @@ class NFA:
         self.tree = tree
         self.regexAccepted = None
 
-        # Propiedades para crear el diagrama
+        # Propiedades para crear el diagrama con Graphviz
         self.dot = Digraph(comment='Diagrama AFN', strict=True)
         self.dot.attr(rankdir='LR')
         self.dot.attr('node', shape='circle')
 
-        # Se ejecuta el algoritmo
+        # Ejecuta el algoritmo de construcción del NFA
         self.Render(tree)
         self.trans_func = self.GenerateTransitionTable()
         self.accepting_states = self.GetAcceptingState()
 
     def Render(self, node):
+        # Renderiza el nodo actual llamando al método correspondiente
         self.prev_state = self.curr_state
         method_name = node.__class__.__name__ + 'Node'
         method = getattr(self, method_name)
         return method(node)
 
     def LetterNode(self, node):
+        # Renderiza un nodo de letra
         return node.value
 
     def AppendNode(self, node):
-
+        # Renderiza un nodo de concatenación (.)
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
             self.Render(node.a)
         )
-
         self.curr_state += 1
         self.dot.edge(
             str(self.curr_state - 1),
@@ -56,18 +50,17 @@ class NFA:
         )
 
     def OrNode(self, node):
+        # Renderiza un nodo OR (+)
         initial_node = self.curr_state - 1
         mid_node = None
 
-        # desde el inicial al primer epsilon
         self.dot.edge(
             str(initial_node),
             str(self.curr_state),
-            'e'
+            'ε'
         )
         self.curr_state += 1
 
-        # de epsilon al primero
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
@@ -77,16 +70,14 @@ class NFA:
         mid_node = self.curr_state
         self.curr_state += 1
 
-        # desde el inicial al segundo epsilon
         self.dot.edge(
             str(initial_node),
             str(self.curr_state),
-            'e'
+            'ε'
         )
 
         self.curr_state += 1
 
-        # desde epsilon al segundo
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
@@ -95,137 +86,62 @@ class NFA:
 
         self.curr_state += 1
 
-        # desde el primero al ultimo epsilon
         self.dot.edge(
             str(mid_node),
             str(self.curr_state),
-            'e'
+            'ε'
         )
 
-        # desde el segundo al ultimo epsilon
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
-            'e'
+            'ε'
         )
 
     def KleeneNode(self, node):
-
-        # Primer epsilon
+        # Renderiza un nodo de estrella de Kleene (*)
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
-            'e'
+            'ε'
         )
 
         first_node = self.curr_state - 1
         self.curr_state += 1
 
-        # Nodo de renderizado
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
             self.Render(node.a)
         )
 
-        # nodo en el último estado al primer epsilon
         self.dot.edge(
             str(self.curr_state),
             str(first_node + 1),
-            'e'
+            'ε'
         )
 
         self.curr_state += 1
 
-        # Segundo epsilon
         self.dot.edge(
             str(self.curr_state - 1),
             str(self.curr_state),
-            'e'
+            'ε'
         )
 
-        # Del primer epsilon al ultimo estado
         self.dot.edge(
             str(first_node),
             str(self.curr_state),
-            'e'
+            'ε'
         )
-
-    def PlusNode(self, node):
-        self.KleeneNode(node)
-        self.curr_state += 1
-
-        self.dot.edge(
-            str(self.curr_state - 1),
-            str(self.curr_state),
-            self.Render(node.a)
-        )
-
-    def QuestionNode(self, node):
-        initial_node = self.curr_state - 1
-        mid_node = None
-
-        # desde el primero al primer epsilon
-        self.dot.edge(
-            str(initial_node),
-            str(self.curr_state),
-            'e'
-        )
-        self.curr_state += 1
-
-        # desde epsilon al primero
-        self.dot.edge(
-            str(self.curr_state - 1),
-            str(self.curr_state),
-            self.Render(node.a)
-        )
-
-        mid_node = self.curr_state
-        self.curr_state += 1
-
-        # desde el primero al segundo epsilon
-        self.dot.edge(
-            str(initial_node),
-            str(self.curr_state),
-            'e'
-        )
-
-        self.curr_state += 1
-
-        # desde epsilon al segundo
-        self.dot.edge(
-            str(self.curr_state - 1),
-            str(self.curr_state),
-            'e'
-        )
-
-        self.curr_state += 1
-
-        # desde el primero al ultimo epsilon
-        self.dot.edge(
-            str(mid_node),
-            str(self.curr_state),
-            'e'
-        )
-
-        # desde el segundo al ultimo epsilon
-        self.dot.edge(
-            str(self.curr_state - 1),
-            str(self.curr_state),
-            'e'
-        )
-
     def GenerateTransitionTable(self):
+        # Genera la tabla de transiciones!
 
-        states = [i.replace('\t', '')
-                  for i in self.dot.source.split('\n') if '->' in i and '=' in i]
+        states = [i.replace('\t', '') for i in self.dot.source.split('\n') if '->' in i and '=' in i] #obtiene las transiciones del diagrama
+        self.trans_func = dict.fromkeys([str(s) for s in range(self.curr_state + 1)])#crea un diccionario con los numeros de los estados
+        self.trans_func[str(self.curr_state)] = dict()#inicia la tabla de transiciones para un estado actual
 
-        self.trans_func = dict.fromkeys(
-            [str(s) for s in range(self.curr_state + 1)])
-
-        self.trans_func[str(self.curr_state)] = dict()
-
-        for state in states:
+        for state in states: #procesa todas las transiciones y actualiza la tabla
             splitted = state.split(' ')
             init = splitted[0]
             final = splitted[2]
@@ -241,25 +157,25 @@ class NFA:
         return self.trans_func
 
     def EvalRegex(self):
+        # Evalúa si la expresión regular es aceptada por el NFA
         try:
             self.EvalNext(self.regex[0], '0', self.regex)
             return 'Yes' if self.regexAccepted else 'No'
         except RecursionError:
-            if self.regex[0] in self.symbols and self.regex[0] != 'e':
+            if self.regex[0] in self.symbols and self.regex[0] != 'ε':
                 return 'Yes'
             else:
                 return 'No'
 
     def EvalNext(self, eval_symbol, curr_state, eval_regex):
-
+        # Evalúa el siguiente símbolo de la expresión regular
         if self.regexAccepted != None:
             return
 
         transitions = self.trans_func[curr_state]
         for trans_symbol in transitions:
-
-            if trans_symbol == 'e':
-                if not eval_regex and str(self.accepting_states) in transitions['e']:
+            if trans_symbol == 'ε':
+                if not eval_regex and str(self.accepting_states) in transitions['ε']:
                     self.regexAccepted = True
                     return
 
@@ -297,19 +213,23 @@ class NFA:
                     self.EvalNext(next_symbol, state, next_regex)
 
     def GetAcceptingState(self):
+        # Obtiene el estado de aceptación
         self.dot.node(str(self.curr_state), shape='doublecircle')
         self.accepting_states.append(self.curr_state)
         return self.curr_state
 
     def WriteNFADiagram(self):
-        source = self.dot.source
-        debug_string = f'''
-NFA:
-- Símbolos: {self.symbols}
-- Estado final: {self.accepting_states}
-- Tabla de transición:
-        '''
-        # print(debug_string)
-        # pprint(self.trans_func)
+        # Escribe el diagrama del NFA en un archivo y lo renderiza
+        source = self.dot.source #obtiene la fuente del diagrama
+        
+        #escribe en el archivo
         WriteToFile('./output/NFA.gv', source)
         self.dot.render('./output/NFA.gv', view=True)
+
+
+
+# Función que escribe contenido en un archivo.
+def WriteToFile(filename: str, content: str):
+    with open(filename, 'w') as _file:
+        _file.write(content)
+    return f'Archivo "{filename}" creado'
