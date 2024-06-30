933af545529c59e258728f63365f5865dddf1e5c
Leonardo Morabito <leonardomorabito02@gmail.com>
2024-06-30 11:01:37 -0300

    elimino codigo innecesario

--- functions/nfa.py
+++ functions/nfa.py
@@ -20,7 +20,7 @@ class NFA:
 
         # Ejecuta el algoritmo de construcción del NFA
         self.Render(tree)
-        self.trans_func = self.GenerateTransitionTable()
+        #self.trans_func = self.GenerateTransitionTable()
         self.accepting_states = self.GetAcceptingState()
 
     def Render(self, node):
@@ -133,84 +133,6 @@ class NFA:
             str(self.curr_state),
             'ε'
         )
-
-    def GenerateTransitionTable(self):
-        # Genera la tabla de transiciones
-        states = [i.replace('\t', '') for i in self.dot.source.split('\n') if '->' in i and '=' in i]  # obtiene las transiciones del diagrama
-        self.trans_func = dict.fromkeys([str(s) for s in range(self.curr_state + 1)])  # crea un diccionario con los números de los estados
-        self.trans_func[str(self.curr_state)] = dict()  # inicia la tabla de transiciones para un estado actual
-
-        for state in states:  # procesa todas las transiciones y actualiza la tabla
-            splitted = state.split(' ')
-            init = splitted[0]
-            final = splitted[2]
-
-            symbol_index = splitted[3].index('=')
-            symbol = splitted[3][symbol_index + 1]
-
-            try:
-                self.trans_func[init][symbol].append(final)
-            except:
-                self.trans_func[init] = {symbol: [final]}
-
-        return self.trans_func
-
-    def EvalRegex(self):
-        # Evalúa si la expresión regular es aceptada por el NFA
-        try:
-            self.EvalNext(self.regex[0], '0', self.regex)
-            return 'Yes' if self.regexAccepted else 'No'
-        except RecursionError:
-            if self.regex[0] in self.symbols and self.regex[0] != 'ε':
-                return 'Yes'
-            else:
-                return 'No'
-
-    def EvalNext(self, eval_symbol, curr_state, eval_regex):
-        # Evalúa el siguiente símbolo de la expresión regular
-        if self.regexAccepted is not None:
-            return
-
-        transitions = self.trans_func[curr_state]
-        for trans_symbol in transitions:
-            if trans_symbol == 'ε':
-                if not eval_regex and str(self.accepting_states) in transitions['ε']:
-                    self.regexAccepted = True
-                    return
-
-                for state in transitions['ε']:
-                    if self.regexAccepted is not None:
-                        break
-                    self.EvalNext(eval_symbol, state, eval_regex)
-
-            elif trans_symbol == eval_symbol:
-                next_regex = eval_regex[1:]
-                try:
-                    next_symbol = next_regex[0]
-                except:
-                    next_symbol = None
-
-                if not next_symbol:
-                    if str(self.accepting_states) in transitions[trans_symbol]:
-                        self.regexAccepted = True
-                        return
-
-                    elif str(self.accepting_states) != curr_state:
-                        for state in transitions[trans_symbol]:
-                            self.EvalNext('ε', state, None)
-                        if self.regexAccepted is not None:
-                            return
-
-                if self.regexAccepted is not None:
-                    return
-
-                for state in transitions[trans_symbol]:
-                    if not next_symbol and str(state) == self.accepting_states:
-                        self.regexAccepted = True
-                        return
-
-                    self.EvalNext(next_symbol, state, next_regex)
-
     def GetAcceptingState(self):
         # Obtiene el estado de aceptación
         self.dot.node(str(self.curr_state), shape='doublecircle')
